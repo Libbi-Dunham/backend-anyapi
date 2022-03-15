2da27b2fcfce0a2b89c929599abe397762734317
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-03-15 15:34:17 -0700

    got my first test/post to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+// const Toy = require('../lib/models/Toys');
 
 describe('backend-anyapi routes', () => {
   beforeEach(() => {
